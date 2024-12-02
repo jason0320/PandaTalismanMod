81499259051b238967339ccfb34598f99a3c4362
jason0320 <jasonnosaj0320@gmail.com>
2024-12-02 13:02:52 +0800

    Update ActThrowThrowPatch.cs

--- ActThrowThrowPatch.cs
+++ ActThrowThrowPatch.cs
@@ -11,7 +11,8 @@ namespace Mod_PandaTalismanMod
 {
 
     [HarmonyPatch]
-    internal class ActThrowThrowPatch {
+    internal class ActThrowThrowPatch
+    {
 
         [HarmonyTranspiler]
         [HarmonyPatch(typeof(ActThrow), nameof(ActThrow.Throw), new Type[] { typeof(Card), typeof(Point), typeof(Card), typeof(Thing), typeof(ThrowMethod) })]
@@ -32,24 +33,28 @@ namespace Mod_PandaTalismanMod
 
         internal static void ProcTalisman(bool flagHit)
         {
-            var weapon = Act.CC.Tool;
-            bool flag3 = true;
+            if (EClass.pc.Tool == null && EClass.pc.TryGetThrowable() == null)
+            {
+                return;
+            }
+            var weapon = EClass.pc.Tool;
+            if (EClass.pc.ai.IsRunning)
+            {
+                weapon = EClass.pc.TryGetThrowable();
+            }
+            bool flag3 = false;
             if (weapon.c_ammo <= 0 || Act.CC.HasCondition<ConReload>())
             {
-                flag3 = false;
                 return;
             }
             if (!(weapon.ammoData?.trait is TraitAmmo traitAmmo && weapon.ammoData?.trait is TraitAmmoTalisman traitAmmoTalisman))
             {
-                flag3 = false;
                 return;
             }
             if (!flagHit || Act.TC == null || !Act.TC.IsAliveInCurrentZone)
             {
-                flag3 = false;
                 return;
             }
-            
             var act = Act.CC.elements.GetElement(traitAmmoTalisman.owner.refVal)?.act ?? ACT.Create(traitAmmoTalisman.owner.refVal);
             Act.powerMod = traitAmmo.owner.encLV;
             var tC = Act.TC;
@@ -61,20 +66,21 @@ namespace Mod_PandaTalismanMod
             }
             Act.TC = tC;
             Act.powerMod = 100;
-            if (flag3)
+            if (!flag3)
             {
-                weapon.c_ammo--;
-                if (weapon.ammoData != null)
-                {
-                    weapon.ammoData.Num = weapon.c_ammo;
-                }
-                if (weapon.c_ammo <= 0)
-                {
-                    weapon.c_ammo = 0;
-                    weapon.ammoData = null;
-                }
-                LayerInventory.SetDirty(weapon);
+                return;
             }
+            weapon.c_ammo--;
+            if (weapon.ammoData != null)
+            {
+                weapon.ammoData.Num = weapon.c_ammo;
+            }
+            if (weapon.c_ammo <= 0)
+            {
+                weapon.c_ammo = 0;
+                weapon.ammoData = null;
+            }
+            LayerInventory.SetDirty(weapon);
         }
     }
 }
